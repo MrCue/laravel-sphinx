aa54245ec2d313172b1fc155660025b378efef88
Steve Greenwood <steve@lythamcomputers.com>
2015-05-28 16:00:27 +0100

    Sphinx search for laravel 5

---
+++ src/ShpinxSearch.php
@@ -0,0 +1,11 @@
+<?php namespace MrCue\LaravelSphinx;
+
+class SphinxSearch
+{
+    protected $_connection;
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ src/SphinxSearchServiceProvider.php
@@ -0,0 +1,11 @@
+<?php namespace MrCue\LaravelSphinx;
+
+class SphinxSearchServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        $this->app['sphinxsearch'] = $this->app->share(function ($app) {
+            return new SphinxSearch;
+        });
+    }
+}
